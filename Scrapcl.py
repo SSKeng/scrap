9c6fd76e9451c91d0000217fa73d738fe8805a52
zss <1141642845@qq.com>
2018-05-11 20:23:44 +0800

    add Scrapcl.py

--- Scrapcl.py
+++ Scrapcl.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
-import requests
 from bs4 import BeautifulSoup
 import time
-import urllib
+import urllib.request
 import os
+import requests
 
 def download_pic(url):
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36')]
+    urllib.request.install_opener(opener)
     split_url = url.split('/')
     filename = split_url.pop()
-    urllib.urlretrieve(url, filename='/home/SSKeng/Desktop/python/img/'+filename)
+    urllib.request.urlretrieve(url, filename='/home/SSKeng/Desktop/python/imgcl/'+filename)
 
 
 def get_page(page_url):
@@ -19,24 +22,18 @@ def get_page(page_url):
 #    print >>f, content
     content = f.read()
     soup = BeautifulSoup(content, 'lxml')
-    img_list =  soup.find_all('img', attrs={"class":"img-responsive lazy image_dta"})
+    img_list =  soup.find_all('input', attrs={"type":"image"})
     for img in img_list:
-        img_url = img['data-original']
+        img_url = img['src']
         download_pic(img_url)
 
-def main():
-    for page in range(1,2):
-        url = BASE_PAGE_URL+str(page)
-        print url
-        get_page(url)
-
 if __name__ == '__main__':
+    BASE_PAGE_URL = 'http://***.html'
     headers = {
-    'Host':'www.doutula.com',
+    'Host':'www.jnpxba.com',
     'Upgrade-Insecure-Requests':'1',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36' } 
-    BASE_PAGE_URL = 'http://www.doutula.com/photo/list/?page='
-    main()
+    get_page(BASE_PAGE_URL)
 
 #--------- input label--------------
 #    img_list =  soup.find_all('input', attrs={'type':'image'})
